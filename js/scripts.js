444f5d1a1015b3dbb227472efb16459e3da53128
Steve Santos <padma@email.com>
2018-09-23 11:01:53 -0700

    Was able to fix my bug by using the method text instead of append so it can replace it when submitting again.  Webpage complete.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(function() {
   $("#formOne").submit(function(event) {
     var inputName = $("#iName").val();
-    $(".name").append(inputName);//need to find a way to replace the last append so it doesnt keep appending names
+    $(".name").text(inputName);//need to find a way to replace the last append so it doesnt keep appending names
     $("#postcard").show();
     $("#postcard").click(function() {
       $("#postcard").hide();
